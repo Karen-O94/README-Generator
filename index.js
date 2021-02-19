2a614ae9481f041576ea19c21bf1817e1bd04f11
Karen Opoku <karenyaa@live.co.uk>
2021-02-19 21:19:39 +0000

    created an array for user to input their answers

--- index.js
+++ index.js
@@ -5,8 +5,58 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = ()=>
     inquirer.prompt([
-        
-    ])
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'aim',
+        message: 'What motivated to build this project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install this project?',
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Enter a url or folder path to add screenshots of your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this?',
+    },
+    {
+        type: 'input',
+        name: 'technology',
+        message: 'What technology was this project built with?',
+    },
+    {
+        type: 'input',
+        name: 'success',
+        message: 'What were the successes of this project?',
+    },
+    {
+        type: 'input',
+        name: 'difficulty',
+        message: 'What were the difficulties of this project?',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Add the creditors of this project',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license was used for this project?',
+    },
+    ]);
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
