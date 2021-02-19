c780214285225f5361a22b42181c97b3ea67e9ce
Karen Opoku <karenyaa@live.co.uk>
2021-02-19 23:39:42 +0000

    function to generate markdown file completed

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,11 +15,11 @@ renderLicenseBadge;
 function renderLicenseLink(license) {
   `#License
   ${data.license}`
-}
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -29,7 +29,7 @@ function generateMarkdown(data) {
   # Motivation
   ${data.motivation}
   #Table of Contents
-  1.[Installation](#Installation)
+  1.[Installation](##Installation)
   2.[Usage](#Usage)
   3.[Technology](#Technology)
   4.[Screenshot](#Screenshot)
